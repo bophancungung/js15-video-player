a2f167fd4bbddd69a1845400ec1ac06af733da53
bophancungung <bophancungung@gmail.com>
2021-08-11 17:23:20 +0700

    js - video play/pause

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
-
+const video = document.querySelector('.video');
+const progressRange = document.querySelector('.progress-range');
+const progressBar = document.querySelector('.progress-bar');
+const playBtn = document.getElementById('play-btn');
+const volumeIcon = document.getElementById('volume-icon');
+const volumeRange = document.querySelector('.volume-range');
+const volumeBar = document.querySelector('.volume-bar');
+// player-speed element???
+const currentTime = document.querySelector('.time-elapsed');
+const duration = document.querySelector('.time-duration');
+const fullscreenBtn = document.getElementById('fullscreen-btn');
 
 // Play & Pause ----------------------------------- //
+function showPlayIcon(isPlaying) {
+  if (isPlaying) {
+    playBtn.setAttribute('title', 'Pause');
+    playBtn.classList.replace('fa-play', 'fa-pause');
+  } else {
+    playBtn.setAttribute('title', 'Play');
+    playBtn.classList.replace('fa-pause', 'fa-play');
+  }
+}
 
+function togglePlay() {
+  if (video.paused) {
+    video.play();
+    showPlayIcon(true);
+  } else {
+    video.pause();
+    showPlayIcon(false);
+  }
+}
+
+// On video end, show play button icon
+video.addEventListener('ended', showPlayIcon(true));
 
 
 // Progress Bar ---------------------------------- //
@@ -19,3 +50,6 @@
 // Fullscreen ------------------------------- //
 
 
+// Event listeners
+playBtn.addEventListener('click', togglePlay);
+video.addEventListener('click', togglePlay);
