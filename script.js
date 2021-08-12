b288a1bb9113625202d2a3a41e200b5e2d71051c
bophancungung <bophancungung@gmail.com>
2021-08-12 10:11:24 +0700

    js - progress bar with dynamic width - populate current time duration - volume bar volume icon

--- script.js
+++ script.js
@@ -31,16 +31,47 @@ function togglePlay() {
   }
 }
 
-// On video end, show play button icon
-video.addEventListener('ended', showPlayIcon(true));
-
 
 // Progress Bar ---------------------------------- //
 
+// Calculate display time format
+function displayTime(duration) {
+  const minutes = Math.floor(duration / 60);
+  let seconds = Math.floor(duration % 60);
+  seconds > 9 ? seconds : `0${seconds}`;
+  return `${minutes}:${seconds}`;
+}
 
+// Update progress bar as video plays
+function updateProgress() {
+  progressBar.style.width = `${video.currentTime / video.duration * 100}%`;
+  currentTime.textContent = `${displayTime(video.currentTime)} / `;
+  duration.textContent = displayTime(video.duration);
+}
+
+function setProgress(e) {
+  newTime = e.offsetX / progressRange.offsetWidth;
+  progressBar.style.width = `${newTime * 100}%`;
+  video.currentTime = newTime * video.duration;
+}
 
 // Volume Controls --------------------------- //
-
+function changeVolume(e) {
+  let volume = e.offsetX / volumeRange.offsetWidth;
+  volume < 0.1 ? volume = 0 : volume;
+  volume > 0.9 ? volume = 1 : volume;
+  video.volume = volume;
+  volumeBar.style.width = `${volume * 100}%`;
+  // Change volume icon
+  volumeIcon.className = '';
+  if (volume > 0.7) {
+    volumeIcon.classList.add('fas', 'fa-volume-up');
+  } else if (volume < 0.7 && volume > 0) {
+    volumeIcon.classList.add('fas', 'fa-volume-down');
+  } else if (volume === 0) {
+    volumeIcon.classList.add('fas', 'fa-volume-off');
+  }
+}
 
 
 // Change Playback Speed -------------------- //
@@ -53,3 +84,9 @@ video.addEventListener('ended', showPlayIcon(true));
 // Event listeners
 playBtn.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay);
+video.addEventListener('timeupdate', updateProgress);
+video.addEventListener('canplay', updateProgress);
+// On video end, show play button icon
+video.addEventListener('ended', () => {showPlayIcon(false)});
+progressRange.addEventListener('click', setProgress);
+volumeRange.addEventListener('click', changeVolume);
